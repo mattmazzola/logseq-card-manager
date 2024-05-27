325df3ff0cc24514f05a699edc355df70e61366a
Matt Mazzola <mattmazzola@live.com>
2024-05-27 13:14:19 -0700

    Remove length logs since already shown

--- src/main.tsx
+++ src/main.tsx
@@ -51,10 +51,10 @@ async function fetchCards() {
     return
   }
 
-  console.log({ queryBlocksCount: queryBlocks.length, queryBlocks })
+  console.log({ queryBlocks })
   const cardBlocks = queryBlocks.filter(block => block.content?.includes('#card') ?? false)
 
-  console.log({ cardBlocksCount: cardBlocks.length, cardBlocks })
+  console.log({ cardBlocks })
   if (cardBlocks.length === 0) {
     console.warn('No card blocks found. Aborting fetch.')
     return
@@ -68,29 +68,59 @@ async function fetchCards() {
   })
   console.log({ cardsWithParsedContent })
 
-  const blocksWithNonCardProperties: any[] = []
-  for (const parsedBlock of cardsWithParsedContent) {
+  const blocksWithNonCardProperties = cardsWithParsedContent.map(parsedBlock => {
     const normalizedProperties = parsedBlock.parsedContent.map(x => x.replace('#', ''))
     const nonCardProperties = normalizedProperties.filter(p => !p.startsWith('card'))
-    const hasNonCardProperty = nonCardProperties.length > 0
     const newBlock = {
       ...parsedBlock,
       nonCardProperties
     }
-    blocksWithNonCardProperties.push(newBlock)
-  }
 
+    return newBlock
+  })
+
+  const UNCATEGORIZED_CATEGORY_NAME = 'uncategorized'
   console.log({ blocksWithNonCardProperties })
   const categoryToBlocksMap = new Map()
   for (const block of blocksWithNonCardProperties) {
-    for (const nonCardProperty of block.nonCardProperties) {
-      const existingBlocks = categoryToBlocksMap.get(nonCardProperty) ?? []
+    if (block.nonCardProperties.length === 0) {
+      console.debug('No non-card properties found. Adding to Uncategorized category.', { block })
+      const categoryName = UNCATEGORIZED_CATEGORY_NAME
+      let existingBlocks = categoryToBlocksMap.get(categoryName)
+      if (!existingBlocks) {
+        existingBlocks = []
+        categoryToBlocksMap.set(categoryName, existingBlocks)
+      }
+
       existingBlocks.push(block)
-      categoryToBlocksMap.set(nonCardProperty, existingBlocks)
+    }
+
+    for (const nonCardProperty of block.nonCardProperties) {
+      const categoryName = nonCardProperty
+      let existingBlocks = categoryToBlocksMap.get(categoryName)
+      if (!existingBlocks) {
+        existingBlocks = []
+        categoryToBlocksMap.set(categoryName, existingBlocks)
+      }
+
+      existingBlocks.push(block)
     }
   }
 
   console.log({ categoryToBlocksMap })
+  const totalCategorizedBlocks = Array.from(categoryToBlocksMap.values()).reduce((acc, blocks) => acc + blocks.length, 0)
+  console.log({ totalCategorizedBlocks })
+
+  // // For each category, transform the value to object with the percentage of blocks in that category
+  // const categoryToBlocksPercentageMap = new Map()
+  // for (const [categoryName, categoryBlocks] of categoryToBlocksMap.entries()) {
+  //   const percentage = categoryBlocks.length / blocksWithNonCardProperties.length
+  //   categoryToBlocksPercentageMap.set(categoryName, {
+  //     blocks: categoryBlocks,
+  //     percentage
+  //   })
+  // }
+  // console.log({ categoryToBlocksPercentageMap })
 
   // const nonCardProperties = cardBlocks.flatMap(block => Object.entries(block.properties ?? {}).filter(([key, value]) => !key.startsWith('card')))
   // console.log({ nonCardProperties })
